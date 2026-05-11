45ffd60ba37935af96a63603696a352d5a55770c
Rikkas <aboobackerrikkasofficial@gmail.com>
2026-05-11 12:07:17 +0530

    Reposition floating buttons to white space on admin pages

--- src/components/SystemTitleBar.tsx
+++ src/components/SystemTitleBar.tsx
@@ -52,7 +52,7 @@ export function SystemTitleBar() {
   return (
     <div className={cn(
       "fixed top-6 left-0 right-0 z-[99999] hidden lg:flex items-center justify-between px-6 pointer-events-none lg:px-20",
-      isAdjustedPage && "md:left-72 md:px-0 md:pl-10 md:pr-12 md:mt-4"
+      isAdjustedPage && "md:left-72 md:px-0 md:pl-24 md:pr-28 md:mt-4"
     )}>
       {/* Glass Back Button */}
       <button
